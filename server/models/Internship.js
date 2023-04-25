9c7d090b99004d198f728b3ed5af9bcc871bbc16
Lordsssss <202143450@cmontmorency.qc.ca>
2023-04-25 08:05:44 -0400

    Internship Schema

---
+++ server/models/Internship.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const internshipSchema = new Schema({
+  contactname: { type: String, required: true },
+  contactemail: { type: String, required: true },
+  contactphone: { type: String, required: true },
+  companyname: { type: String, required: true },
+  companyadresse: { type: String, required: true },
+  internshiptype: { type: String, required: true },
+  nbpositions: { type: Number, required: true },
+  internshipdescription: { type: String, required: true },
+  salary: { type: String, required: true },
+  ownerid: { type: mongoose.Types.ObjectId, red: "OwnerId" },
+});
+
+module.exports = mongoose.model("Internship", userSchema);
