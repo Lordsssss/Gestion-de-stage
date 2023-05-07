c412c390e78a715fb9615cd24e285468aff455c7
Lordsssss <202143450@cmontmorency.qc.ca>
2023-05-07 00:00:10 -0400

    maybe this time 6

--- server/index.js
+++ server/index.js
@@ -13,17 +13,14 @@ const InternshipRoute = require("./routes/InternshipRoute");
 const EmailSysController = require("./routes/EmailSysRoute");
 const StudentRoute = require("./routes/StudentRoute");
 
-const corsOptions = {
-  origin: "https://gestion-stage.vercel.app",
-  optionsSuccessStatus: 200,
-};
+
 
 
 app.use(express.urlencoded());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api/email", EmailSysController);
 app.use("/api/user", UserRoute);
